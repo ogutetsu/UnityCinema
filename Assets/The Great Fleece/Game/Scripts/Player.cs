093ad6ccc74e48d536129c3a89440a22b20d55f5
og <oguogu@test.com>
2020-03-18 16:43:18 +0900

    [update] PlayerのIdleとWalkアニメの切り替えを実装

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 using UnityEngine.AI;
 
@@ -7,11 +8,14 @@ public class Player : MonoBehaviour
 {
 
     private NavMeshAgent _agent;
+    private Animator _anim;
+    private Vector3 _target;
     
     // Start is called before the first frame update
     void Start()
     {
         _agent = GetComponent<NavMeshAgent>();
+        _anim = GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -23,10 +27,18 @@ public class Player : MonoBehaviour
             RaycastHit hitInfo;
             if (Physics.Raycast(rayOrigin, out hitInfo))
             {
-                Debug.Log("Hit: " + hitInfo.point);
-
+  
                 _agent.SetDestination(hitInfo.point);
+                _anim.SetBool("Walk", true);
+                _target = hitInfo.point;
             }
         }
+
+        float distance = Vector3.Distance(transform.position, _target);
+        if (distance < 1.0f)
+        {
+            _anim.SetBool("Walk", false);
+        }
+
     }
 }
