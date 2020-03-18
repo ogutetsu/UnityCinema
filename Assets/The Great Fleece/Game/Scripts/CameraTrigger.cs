7ac6cc7c5173fbc519b617f4efa967222b34ba37
og <oguogu@test.com>
2020-03-18 16:14:25 +0900

    [update] CameraTriggerに切り替え先のカメラを設定

--- Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
+++ Assets/The Great Fleece/Game/Scripts/CameraTrigger.cs
@@ -1,9 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraTrigger : MonoBehaviour
 {
-    
-    
+
+    public Transform myCamera;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Debug.Log("Trigger Activated");
+            Camera.main.transform.position = myCamera.transform.position;
+            Camera.main.transform.rotation = myCamera.transform.rotation;
+        }
+    }
 }
