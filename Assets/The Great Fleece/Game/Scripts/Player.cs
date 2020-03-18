1aec233d79207b00aeea18bafd42f53ea39c4b2f
og <oguogu@test.com>
2020-03-18 15:50:39 +0900

    [update] クリックした座標にプレイヤーを移動させるようにしました。

--- Assets/The Great Fleece/Game/Scripts/Player.cs
+++ Assets/The Great Fleece/Game/Scripts/Player.cs
@@ -1,18 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Player : MonoBehaviour
 {
+
+    private NavMeshAgent _agent;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        _agent = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if (Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                Debug.Log("Hit: " + hitInfo.point);
+
+                _agent.SetDestination(hitInfo.point);
+            }
+        }
     }
 }
