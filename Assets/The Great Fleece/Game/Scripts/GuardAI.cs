017ad8cdaa9299f070fa295b4ac9dbbb9118e0b1
og <oguogu@test.com>
2020-03-25 14:45:12 +0900

    [update] GuardのPointAまでの移動を実装

--- Assets/The Great Fleece/Game/Scripts/GuardAI.cs
+++ Assets/The Great Fleece/Game/Scripts/GuardAI.cs
@@ -1,14 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class GuardAI : MonoBehaviour
 {
     public List<Transform> wayPoints;
+    public Transform currentTarget;
+
+    private NavMeshAgent agent;
     // Start is called before the first frame update
     void Start()
     {
-        
+        agent = GetComponent<NavMeshAgent>();
+        if (wayPoints.Count > 0)
+        {
+            if (wayPoints[0] != null)
+            {
+                currentTarget = wayPoints[0];
+                agent.SetDestination(currentTarget.position);
+            }
+        }
     }
 
     // Update is called once per frame
