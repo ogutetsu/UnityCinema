9ba06b1d461c908e2e85da91449c8c36a485ff20
og <oguogu@test.com>
2020-03-26 13:27:34 +0900

    [update] Eyeコリジョン内に入ったらGameOverCutsceneをAcitiveにするようにしました。 MainCameraの初期Transformを設定

--- Assets/The Great Fleece/Game/Scripts/Eyes.cs
+++ Assets/The Great Fleece/Game/Scripts/Eyes.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Eyes : MonoBehaviour
 {
+
+    public GameObject gameOverCutscene;
     
-    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            gameOverCutscene.SetActive(true);
+        }
+    }
 }
--- Assets/The Great Fleece/Game/Scripts/LookAtPlayer.cs
+++ Assets/The Great Fleece/Game/Scripts/LookAtPlayer.cs
@@ -1,11 +1,19 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour
 {
     public Transform target;
-    
+
+    public Transform startCamera;
+    private void Start()
+    {
+        transform.position = startCamera.position;
+        transform.rotation = startCamera.rotation;
+    }
+
     // Update is called once per frame
     void Update()
     {
