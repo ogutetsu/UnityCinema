8566a103becde114e687e3effe50eef8abf33190
og <oguogu@test.com>
2020-03-27 11:42:10 +0900

    [update] SecurityCameraのコリジョンに入ったらカラーを緑から赤に変更するようにしました。

--- Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
+++ Assets/The Great Fleece/Game/Scripts/SecurityCamera.cs
@@ -10,6 +10,9 @@ public class SecurityCamera : MonoBehaviour
     {
         if (other.tag == "Player")
         {
+            MeshRenderer render = GetComponent<MeshRenderer>();
+            Color color = new Color(0.6f,0.11f,0.1f,0.039f);
+            render.material.SetColor("_TintColor", color);
             gameOverCutscene.SetActive(true);
         }
     }
