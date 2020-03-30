b9c43b07c1f040a4aaff6be544cfc5d073969f8a
og <oguogu@test.com>
2020-03-31 08:39:32 +0900

    [update] UIManagerクラスにRestartとQuit関数を追加

--- Assets/The Great Fleece/Game/Scripts/UIManager.cs
+++ Assets/The Great Fleece/Game/Scripts/UIManager.cs
@@ -1,8 +1,32 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    private static UIManager instance;
+
+    public static UIManager Instance
+    {
+        get => instance;
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
     
 }
