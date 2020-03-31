9a93f6401294f63bf5752c5c9b5684c8fa27f869
og <oguogu@test.com>
2020-03-31 09:46:43 +0900

    [update] 非同期でのシーンロードを実装

--- Assets/The Great Fleece/Game/Scripts/Menu/LoadLevel.cs
+++ Assets/The Great Fleece/Game/Scripts/Menu/LoadLevel.cs
@@ -1,18 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadLevel : MonoBehaviour
 {
+    public Image progressBar;
     // Start is called before the first frame update
     void Start()
     {
-        
+        StartCoroutine(LoadLevelAsync());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator LoadLevelAsync()
     {
-        
+        yield return null;
+
+        AsyncOperation operation = SceneManager.LoadSceneAsync("Main");
+
+        while (!operation.isDone)
+        {
+            progressBar.fillAmount = operation.progress;
+            yield return new WaitForEndOfFrame();
+        }
+
     }
 }
